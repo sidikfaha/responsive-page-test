6f1c3b90123076ab819a9027ba685a2ae25a7f2f
sidikfaha <joefaha1@gmail.com>
2023-07-03 18:55:36 +0000

    fix: disable auto dark mode detect

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Register',
+    description: 'Register',
+  }
+
+export default function Register() {
+    return <>
+        <div>
+            Register page
+        </div>
+    </>
+}
